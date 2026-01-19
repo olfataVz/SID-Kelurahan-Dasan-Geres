50b5a89a3f89defd87ac92955499e294bb02f0cf
olfa <olfatavz@gmail.com>
2026-01-19 19:31:52 +0800

    revision requirements

--- apps.py
+++ apps.py
@@ -1,8 +1,12 @@
+import matplotlib
 import streamlit as st
 import pandas as pd
 import numpy as np
 
 import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use("Agg")
+
 import seaborn as sns
 import plotly.express as px
 
